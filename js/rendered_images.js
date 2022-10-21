37655342fb4a7820a58e95d1d27f73eadd40efd8
einszwaidrei <vangel1208@gmail.com>
2022-10-22 02:10:41 +0500

    Отображены фотографии других пользователей

---
+++ js/rendered_images.js
@@ -0,0 +1,23 @@
+import {photos} from '/photos.js';
+
+const pictures = document.querySelector('.pictures');
+const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
+
+const fragment = document.createDocumentFragment();
+
+const renderPhoto = (photo) => {
+  const element= pictureTemplate.cloneNode(true);
+  element.src = photo.url;
+  element.querySelector('.picture__likes').textContent=photo.likes.length();
+  element.querySelector('.picture__comments').textContent=photo.comments.length();
+  return element;
+};
+
+const renderPhotos = () => {
+  photos.array.forEach((photo) => {
+    fragment.appendChild(renderPhoto(photo));
+  });
+  pictures.appendChild(fragment);
+};
+
+renderPhotos();
